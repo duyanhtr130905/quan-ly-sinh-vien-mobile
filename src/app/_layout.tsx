75e44838546b4c7f3c1056748f540b523a133296
Duy Anh Tran <tranduyanhk101lhp@gmail.com>
2026-08-11 16:54:36 +0700

    fix: start app at tabs navigator

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -17,11 +17,11 @@ export default function RootLayout() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="classes/import" options={{ title: 'Nhập lớp' }} />
           <Stack.Screen name="classes/copy-preview" options={{ title: 'Sao chép lớp' }} />
           <Stack.Screen name="students/import" options={{ title: 'Nhập sinh viên' }} />
           <Stack.Screen name="students/copy-preview" options={{ title: 'Sao chép sinh viên' }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="classes/new" options={{ title: 'Thêm lớp' }} />
           <Stack.Screen name="classes/[id]" options={{ title: 'Chi tiết lớp' }} />
           <Stack.Screen name="classes/[id]/edit" options={{ title: 'Sửa lớp' }} />
